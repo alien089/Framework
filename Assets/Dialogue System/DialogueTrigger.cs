055bd5c026546874c2448766e340664f39126349
Alien089 <gabri.moratelli@gmail.com>
2023-09-07 16:23:30 +0200

    fixed dialogue trigger

--- Assets/Dialogue System/DialogueTrigger.cs
+++ Assets/Dialogue System/DialogueTrigger.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour {
+	[SerializeField] private DialogueManager m_DialogueManager;
 	/// <summary>
 	/// method used to trigger a dialogue, need the index of the specified dialogue
 	/// </summary>
 	/// <param name="indexDialogue"></param>
 	public void TriggerDialogue (int indexDialogue)
 	{
-		//GameManager.instance.DialogueManager.StartDialogue(indexDialogue);
+        m_DialogueManager.StartDialogue(indexDialogue);
 	}
 }
